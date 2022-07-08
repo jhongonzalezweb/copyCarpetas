17ad4d2556519b7bb3b8443404d6333784f6194d
Jhon Gonzalez <66422237+jhongonzalezweb@users.noreply.github.com>
2022-07-08 13:23:47 -0400

    Update copiador.py

--- copiador.py
+++ copiador.py
@@ -16,13 +16,11 @@ def my_function(q):
     rt_org = r"C:\Users\jhong\Desktop\IDLICITACION"
     for i in range(q):
         try:
-            rt_dst = r"C:\Users\jhong\Documents\IDLICITACION{}-{}".format(
-                formato, str(i+1))
+            rt_dst = r"C:\Users\jhong\Documents\IDLICITACION{}-{}".format(formato, str(i+1))
             shutil.copytree(rt_org, rt_dst)
             print("Directorio Creado: {}".format(i+1))
         except:
             print("Directorio existe: {}".format(i+1))
-
 
 if __name__ == "__main__":
     my_function(q)
